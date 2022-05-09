57ca81c93024d27f84cffa9e569e7a4e04835c91
TheBlueTiki <102342287+TheBlueTiki@users.noreply.github.com>
2022-05-09 17:12:32 -0400

    Add files via upload

--- Chess/Chess/Bishop.cpp
+++ Chess/Chess/Bishop.cpp
@@ -11,7 +11,32 @@ Bishop::Bishop()
     SetNewPosition(GetSpritePosition());
 }
 
-void Bishop::UpdateValidMoves(int a_size, Piece* a_pieces[])
+/**/
+/*
+Bishop::UpdateValidMoves()
+
+NAME
+
+        Bishop::UpdateValidMoves - updates the bishop's valid move set based on the current board conditions
+
+SYNOPSIS
+
+        void Bishop::UpdateValidMoves(const int a_size, Piece* a_pieces[]);
+            a_pieces  --> the set of all pieces on the board
+            a_size  --> the size of the grid squares
+
+DESCRIPTION
+
+        This function will create a valid moveset for the bishop based on its current position and the
+        positions of the other pieces on the board
+
+RETURNS
+
+        void
+
+*/
+/**/
+void Bishop::UpdateValidMoves(const int a_size, Piece* a_pieces[])
 {
     //update the old position
     SetOldPosition(GetSpritePosition());
@@ -23,12 +48,68 @@ void Bishop::UpdateValidMoves(int a_size, Piece* a_pieces[])
     std::string oldPos = ToChessNote(GetOldPosition(), a_size);
     AddDiagonalMoves(oldPos, a_pieces, a_size, m_moves);
 }
+/*void Bishop::UpdateValidMoves(const int a_size, Piece* a_pieces[])*/
 
+/**/
+/*
+Bishop::ValidMovesAt()
+
+NAME
+
+        Bishop::ValidMovesAt - generates valid moves at a given position
+
+SYNOPSIS
+
+        void Bishop::ValidMovesAt(std::string a_pos, Piece* a_pieces[], const int a_size, std::vector<std::string>& a_moves);
+            a_pos  --> the starting position to generate the moves from
+            a_pieces  --> the set of all pieces on the board
+            a_size  --> the size of the grid squares
+            a_moves --> the list of moves being added to
+
+DESCRIPTION
+
+        This function will generate moves for the bishop to a specified list based on the given position and the
+        positions of the other pieces on the board.
+
+RETURNS
+
+        void
+
+*/
+/**/
 void Bishop::ValidMovesAt(std::string a_pos, Piece* a_pieces[], const int a_size, std::vector<std::string>& a_moves)
 {
     AddDiagonalMoves(a_pos, a_pieces, a_size, a_moves);
 }
+/*void Bishop::ValidMovesAt(std::string a_pos, Piece* a_pieces[], const int a_size, std::vector<std::string>& a_moves)*/
 
+/**/
+/*
+Bishop::AddDiagonalMoves()
+
+NAME
+
+        Bishop::AddDiagonalMoves - generates diagonal moves at a given position
+
+SYNOPSIS
+
+        void Bishop::AddDiagonalMoves(std::string a_pos, Piece* a_pieces[], const int a_size, std::vector<std::string>& a_moves);
+            a_pos  --> the starting position to generate the moves from
+            a_pieces  --> the set of all pieces on the board
+            a_size  --> the size of the grid squares
+            a_moves --> the list of moves being added to
+
+DESCRIPTION
+
+        This function will generate diagonal moves for the bishop to a specified list based on the given position and the
+        positions of the other pieces on the board.
+
+RETURNS
+
+        void
+
+*/
+/**/
 void Bishop::AddDiagonalMoves(std::string a_pos, Piece* a_pieces[], const int a_size, std::vector<std::string>& a_moves)
 {
     std::string newPos = "";
@@ -40,6 +121,7 @@ void Bishop::AddDiagonalMoves(std::string a_pos, Piece* a_pieces[], const int a_
         newPos += char(a_pos[0] + k);
         newPos += char(a_pos[1] + k);
 
+        //check if the move is out of bounds or obstructed
         if (!IsOutOfBounds(newPos) && (!IsObstructed(newPos, a_pieces, a_size, a_moves) || newPos == ToChessNote(GetOldPosition(), a_size))) {
             a_moves.push_back(newPos);
             k++;
@@ -58,6 +140,7 @@ void Bishop::AddDiagonalMoves(std::string a_pos, Piece* a_pieces[], const int a_
         newPos += char(a_pos[0] - k);
         newPos += char(a_pos[1] + k);
 
+        //check if the move is out of bounds or obstructed
         if (!IsOutOfBounds(newPos) && (!IsObstructed(newPos, a_pieces, a_size, a_moves) || newPos == ToChessNote(GetOldPosition(), a_size))) {
             a_moves.push_back(newPos);
             k++;
@@ -76,6 +159,7 @@ void Bishop::AddDiagonalMoves(std::string a_pos, Piece* a_pieces[], const int a_
         newPos += char(a_pos[0] + k);
         newPos += char(a_pos[1] - k);
 
+        //check if the move is out of bounds or obstructed
         if (!IsOutOfBounds(newPos) && (!IsObstructed(newPos, a_pieces, a_size, a_moves) || newPos == ToChessNote(GetOldPosition(), a_size))) {
             a_moves.push_back(newPos);
             k++;
@@ -94,6 +178,7 @@ void Bishop::AddDiagonalMoves(std::string a_pos, Piece* a_pieces[], const int a_
         newPos += char(a_pos[0] - k);
         newPos += char(a_pos[1] - k);
 
+        //check if the move is out of bounds or obstructed
         if (!IsOutOfBounds(newPos) && (!IsObstructed(newPos, a_pieces, a_size, a_moves) || newPos == ToChessNote(GetOldPosition(), a_size))) {
             a_moves.push_back(newPos);
             k++;
@@ -106,3 +191,4 @@ void Bishop::AddDiagonalMoves(std::string a_pos, Piece* a_pieces[], const int a_
         newPos = "";
     }
 }
+/*void Bishop::AddDiagonalMoves(std::string a_pos, Piece* a_pieces[], const int a_size, std::vector<std::string>& a_moves)*/
